38f757416207f05051144a60fbd849d1c1613f14
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2019-02-04 17:56:10 +0900

    点を追加する仕様を大幅に変更

--- movingPoints/src/script.js
+++ movingPoints/src/script.js
@@ -1,64 +1,26 @@
 'use strict';
-let edgeData = [];
-let segmentData = [];
-let intersectionData = [];
-let intervalData = [];
-let edges = []; // ベクトルの集合
-let graph;
-const EDGE_NUM = 4;
-const SEGMENT_NUM = 4;
+let graph; // graphのグローバルクラスをひとつだけ用意する
 
 let points = []; // 点の集合
 
 function setup(){
   createCanvas(200, 200);
-  graph = createGraphics(200, 200);
-  loadGraph();
+  graph = new graphData();
+  graph.loadGraph();
+  graph.makeGraphic();
   points.push(new point(0, 1, color('blue')));
   points.push(new point(1, 2, color('red')));
   points.push(new point(2, 1, color('green')));
-  points[0].setting();
-  points[1].setting();
-  points[2].setting();
+  points.push(new point(7, 2, color('black')));
 }
 
 function draw(){
   background(240);
-  image(graph, 0, 0);
+  image(graph.graphic, 0, 0);
   points.forEach(function(p){
     p.update();
     p.draw();
   })
-}
-
-function loadGraph(){
-  // とりあえず4つのエッジと4本の辺
-  edgeData = [[40, 40], [160, 40], [40, 160], [160, 160]];
-  console.log(edgeData);
-  segmentData = [[0, 1], [1, 3], [0, 2], [2, 3]];
-  let preData = [[1, 0, 2], [2, 0, 1],[0, 1, 3], [3, 1, 0], [0, 2, 3], [3, 2, 0], [2, 3, 1], [1, 3, 2], [0, 0, 1], [1, 1, 3], [2, 2, 0], [3, 3, 2]]; // スタート直後の進む方向も入れとく
-  preData.forEach(function(data){
-    intersectionData[data[0] * EDGE_NUM + data[1]] = data[2]; // ハッシュ化
-  })
-  // ↑[i, j, k]としてiからjに来たとき次はkみたいな
-  //console.log(edgeData[0]);
-  for(let i = 0; i < edgeData.length; i++){
-    let v = createVector(edgeData[i][0], edgeData[i][1]);
-    console.log(v);
-    edges.push(v);
-  }
-  segmentData.forEach(function(data){ // 面倒だから全部120で
-    intervalData[data[0] * EDGE_NUM + data[1]] = 120;
-    intervalData[data[1] * EDGE_NUM + data[0]] = 120;
-  })
-  segmentData.forEach(function(data){
-    graph.line(edgeData[data[0]][0], edgeData[data[0]][1], edgeData[data[1]][0], edgeData[data[1]][1]);
-  })
-  graph.fill(255);
-  edgeData.forEach(function(data){
-    graph.ellipse(data[0], data[1], 10, 10);
-  })
-  graph.noFill();
 }
 
 class counter{
@@ -78,35 +40,70 @@ class counter{
   increase(){
     if(this.isOn){
       this.count += this.increaseValue;
+      // ここに if(this.count > this.limit){ this.count = this.limit; }を挟むと
+      // this.limitのときにもtrueで実行されるようになるけどoptionだね
+      // この仕様変更により、increaseValueでlimitが割り切れなくても、
+      // もっというとincreaseValueが小数とかでもちゃんとedgeで方向が変化するようになった（さっき知った）
       if(this.count > this.limit){ this.isOn = false; }
     }
     return this.isOn;
   }
 }
 // カウンターを派生させて無限ループとか表現できるようにできたりするかも（カウントリセットするかどうかをオプション化）
-// 簡単なのはthis.loop = true/false だけれど
 
-// pointはどこかのedgeに出現して、その直後にsegmentをintersection経由で与えられて
-// 動き始める感じ。
-class point{
-  constructor(index, speed, color){
-    this.startIndex = index;
-    this.endIndex = index;
-    this.position = createVector(edges[index].x, edges[index].y); // edgeの位置からスタート
-    this.speed = speed; // 1~5くらいを想定
-    this.posCounter = new counter();
-    this.segment;
-    this.color = color;
+class graphData{
+  constructor(){
+    this.edges = [];
+    console.log(this.edges);
+    this.segments = [];
+    this.graphic = createGraphics(width, height);
   }
-  // constructorに初期のスタートとゴールのindex渡して1回目のsettingをconstructorで行うのもあり
-  // そうすれば反対方向に移動させたりできるでしょ、あるいはもっと処理を分割する。
+  loadGraph(){
+    let edgeData = [[40, 40], [160, 40], [160, 160], [40, 160]]; // 0 baseで頂点の集合
+    let inData = [[3, 7], [0, 6], [1, 5], [2, 4]]; // 各頂点に集まる辺のindex
+    let outData = [[0, 4], [1, 7], [2, 6], [3, 5]]; // 出ていく方-
+    for(let index = 0; index < edgeData.length; index++){
+      //console.log(this.edges);
+      this.edges.push(new edge(index, edgeData[index][0], edgeData[index][1], inData[index]));
+      this.edges[index].setConvertData(outData[index]);
+    }
+    let itvData = [[0, 1, 120], [1, 2, 120], [2, 3, 120], [3, 0, 120], [0, 3, 120], [3, 2, 120], [2, 1, 120], [1, 0, 120]]; // 各辺のどの頂点から・・ってのといわゆるcoverに必要なvalue
+    for(let index = 0; index < itvData.length; index++){
+      let data = itvData[index];
+      this.segments.push(new segment(index, this.edges[data[0]], this.edges[data[1]], data[2]));
+    }
+  }
+  makeGraphic(){
+    let lineData = [[0, 1], [1, 2], [2, 3], [3, 0]]; // 辺のデータ
+    for(let i = 0; i < lineData.length; i++){
+      let e1 = this.edges[lineData[i][0]];
+      let e2 = this.edges[lineData[i][1]];
+      this.graphic.line(e1.x, e1.y, e2.x, e2.y);
+    }
+    for(let index = 0; index < this.edges.length; index++){
+      let e = this.edges[index];
+      this.graphic.ellipse(e.x, e.y, 10, 10);
+    }
+  }
+  getEdge(index){ return this.edges[index]; }
+  getSegment(index){ return this.segments[index]; }
+}
+
+// pointはsegmentIndexを与えられてそこからスタートするイメージ
+class point{
+  constructor(segmentIndex, speed, color){
+    this.segment = graph.getSegment(segmentIndex); // indexとグラフの情報からsegmentを取得
+    this.position = createVector(this.segment.start.x, this.segment.start.y); // 初期位置
+    this.color = color;
+    this.speed = speed;
+    this.posCounter = new counter(); // セルフカウンター
+    this.posCounter.setCounter(this.segment.interval, this.speed); // 初期設定
+  }
   setting(){
-    let nextEdgeIndex = intersectionData[this.startIndex * EDGE_NUM + this.endIndex];
-    this.startIndex = this.endIndex;
-    this.endIndex = nextEdgeIndex;
-    let itv = intervalData[this.startIndex * EDGE_NUM + this.endIndex];
-    this.segment = new segment(edges[this.startIndex], edges[this.endIndex], itv);
-    this.posCounter.setCounter(itv, this.speed);
+    // segmentのendであるedgeによりinのsegmentIndexをoutのsegmentIndexに更新
+    let outSegmentIndex = this.segment.end.convertIndex(this.segment.index);
+    this.segment = graph.getSegment(outSegmentIndex);
+    this.posCounter.setCounter(this.segment.interval, this.speed);
   }
   update(){
     if(this.posCounter.increase()){
@@ -124,8 +121,29 @@ class point{
   }
 }
 
+// まずデータからedgeを作り、次にedgeを元にしてitvDataからsegmentを生成する
+class edge{
+  constructor(i, x, y, inArray){
+    this.index = i;
+    this.x = x;
+    this.y = y;
+    this.in = inArray; // 入ってくるsegmentのindex番号
+    this.convert = []; // segmentの変更を司る
+  }
+  setConvertData(outArray){
+    for(let k = 0; k < this.in.length; k++){
+      this.convert[this.in[k]] = outArray[k]; // というわけで、inとoutという2つの配列から作ると。
+    } // これを使うと、outとして新しいsetを渡すことでconvertの仕様を変更できるようになる。
+  }
+  convertIndex(inSegmentIndex){ // inSegment → outSegment
+    return this.convert[inSegmentIndex];
+  }
+}
+
+// startからendまでitvカウントで到達
 class segment{
-  constructor(e1, e2, itv){
+  constructor(i, e1, e2, itv){
+    this.index = i;
     this.start = e1;
     this.end = e2;
     this.interval = itv;
@@ -133,12 +151,7 @@ class segment{
   calcPos(position, cnt){
     let vx = this.start.x + (this.end.x - this.start.x) * (cnt / this.interval);
     let vy = this.start.y + (this.end.y - this.start.y) * (cnt / this.interval);
-    if(cnt === this.interval){ vx = this.end.x; vy = this.end.y; }
+    if(cnt >= this.interval){ vx = this.end.x; vy = this.end.y; }
     position.set(vx, vy);
   }
-}
-
-function keyTyped(){
-  console.log(p.position);
-  console.log(edges[0]);
 }
--- movingPoints/src/script2.js
+++ movingPoints/src/script2.js
@@ -4,23 +4,32 @@ let graph; // graphのグローバルクラスをひとつだけ用意する
 let points = []; // 点の集合
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(160, 160);
   graph = new graphData();
-  graph.loadGraph();
-  graph.makeGraphic();
-  points.push(new point(0, 1, color('blue')));
-  points.push(new point(1, 2, color('red')));
-  points.push(new point(2, 1, color('green')));
-  points.push(new point(7, 2, color('black')));
+  loadData();   // グラフ作る
+  makePoints(); // 点を置く
+  graph.makeGraph() // グラフ生成する
 }
 
 function draw(){
-  background(240);
+  background(220);
   image(graph.graphic, 0, 0);
   points.forEach(function(p){
     p.update();
     p.draw();
   })
+}
+
+function loadData(){
+  // グラフを作りましょう
+  graph.loadEdges([40, 40, 120, 120], [40, 120, 40, 120]);
+  graph.loadSegments([0, 2, 3, 1], [2, 3, 1, 0])
+}
+
+function makePoints(){
+  // とりあえず点をたくさん作りましょう
+  let eset = graph.edges;
+  points.push(new point(eset[0], eset[1], 1, 'red'));
 }
 
 class counter{
@@ -40,58 +49,95 @@ class counter{
   increase(){
     if(this.isOn){
       this.count += this.increaseValue;
-      // ここに if(this.count > this.limit){ this.count = this.limit; }を挟むと
-      // this.limitのときにもtrueで実行されるようになるけどoptionだね
+      // この仕様変更により、increaseValueでlimitが割り切れなくても、
+      // もっというとincreaseValueが小数とかでもちゃんとedgeで方向が変化するようになった（さっき知った）
       if(this.count > this.limit){ this.isOn = false; }
     }
     return this.isOn;
   }
 }
-// カウンターを派生させて無限ループとか表現できるようにできたりするかも（カウントリセットするかどうかをオプション化）
 
+// グラフのデータ色々（グラフ自体の描画とか・・頂点や辺の追加と削除もここで行う）
 class graphData{
   constructor(){
     this.edges = [];
-    console.log(this.edges);
     this.segments = [];
     this.graphic = createGraphics(width, height);
   }
-  loadGraph(){
-    let edgeData = [[40, 40], [160, 40], [160, 160], [40, 160]];
-    let inData = [[3, 7], [0, 6], [1, 5], [2, 4]];
-    let outData = [[0, 4], [1, 7], [2, 6], [3, 5]];
-    for(let index = 0; index < edgeData.length; index++){
-      //console.log(this.edges);
-      this.edges.push(new edge(index, edgeData[index][0], edgeData[index][1], inData[index]));
-      this.edges[index].setConvertData(outData[index]);
-    }
-    let itvData = [[0, 1, 120], [1, 2, 120], [2, 3, 120], [3, 0, 120], [0, 3, 120], [3, 2, 120], [2, 1, 120], [1, 0, 120]];
-    for(let index = 0; index < itvData.length; index++){
-      let data = itvData[index];
-      this.segments.push(new segment(index, this.edges[data[0]], this.edges[data[1]], data[2]));
-    }
+  addEdge(x, y){
+    // 同じ頂点は追加できないようにしたいけどな・・(バリデーション)
+    this.edges.push(new edge(x, y));
+    console.log(x, y);
   }
-  makeGraphic(){
-    let lineData = [[0, 1], [1, 2], [2, 3], [3, 0]]; // この場合は最大で6本引ける
-    for(let i = 0; i < lineData.length; i++){
-      let e1 = this.edges[lineData[i][0]];
-      let e2 = this.edges[lineData[i][1]];
-      this.graphic.line(e1.x, e1.y, e2.x, e2.y);
+  getEdgeIndex(edge){
+    for(let i = 0; i < this.edges.length; i++){
+      if(this.edges[i].index === edge.index){ return i; }
     }
-    for(let index = 0; index < this.edges.length; index++){
-      let e = this.edges[index];
+    return -1;
+  }
+  addSegment(e1, e2){
+    let e1Index = this.getEdgeIndex(e1);
+    let e2Index = this.getEdgeIndex(e2);
+    if(e1Index < 0 || e2Index < 0){ return; } // 存在しない場合
+    let newSegment = new segment(e1, e2); // 新しいsegmentができました
+    this.segments.push(newSegment);
+    e1.addSegment(newSegment); // リストの更新（忘れてた。。。）
+    e2.addSegment(newSegment);
+    //this.segments.push(new segment(e1, e2));
+    //console.log(e1);
+    //console.log(e2);
+  }
+  getSegmentIndex(e1, e2){
+    for(let i = 0; i < this.segments.length; i++){
+      let s = this.segments[i];
+      if(s.start.index === e1.index && s.goal.index === e2.index){ return i; }
+      if(s.start.index === e2.index && s.goal.index === e1.index){ return i; }
+    }
+    return -1;
+  }
+  getSegment(e1, e2){
+    let segmentIndex = this.getSegmentIndex(e1, e2);
+    if(segmentIndex < 0){ return None; }
+    let s = this.segments[segmentIndex];
+    return s;
+  }
+  loadEdges(edgeDataX, edgeDataY){
+    // edgeDataX, edgeDataYは頂点のx座標リスト、y座標リスト
+    for(let i = 0; i < edgeDataX.length; i++){ this.addEdge(edgeDataX[i], edgeDataY[i]); }
+  }
+  loadSegments(e1Data, e2Data){
+    // e1Dataの点からe2Dataの点に線を引くイメージ(indexの配列)
+    //console.log(e1Data);
+    //console.log(e2Data);
+    //console.log(e1Data.length);
+    //console.log(this.edges);
+    for(let i = 0; i < e1Data.length; i++){ this.addSegment(this.edges[e1Data[i]], this.edges[e2Data[i]]); }
+  }
+  makeGraph(){
+    // edgeとsegmentのデータからグラフを生成
+    this.segments.forEach(function(s){
+      this.graphic.line(s.start.x, s.start.y, s.goal.x, s.goal.y);
+    }, this)
+    this.edges.forEach(function(e){
       this.graphic.ellipse(e.x, e.y, 10, 10);
-    }
+    }, this)
   }
-  getEdge(index){ return this.edges[index]; }
-  getSegment(index){ return this.segments[index]; }
+  // そのうちアクティブに追加できるようにしたいけどね
+  // そうなるとedge固有のindexとか連番じゃなくなったりする可能性があるから区別してる
 }
 
 // pointはsegmentIndexを与えられてそこからスタートするイメージ
 class point{
-  constructor(segmentIndex, speed, color){
-    this.segment = graph.getSegment(segmentIndex); // indexとグラフの情報からsegmentを取得
-    this.position = createVector(this.segment.start.x, this.segment.start.y); // 初期位置
+  constructor(e1, e2, speed, color){
+    this.segment = graph.getSegment(e1, e2); // e1からe2に向かう点を生成する
+    // moveFlagはthis.segmentのstartとe1を比べるだけでいいよね？
+    this.moveFlag = (this.segment.start.index === e1.index ? true : false);
+    this.position = createVector(0, 0); // 初期位置
+    if(this.moveFlag){
+      this.position.set(this.segment.start.x, this.segment.start.y); // from start
+    }else{
+      this.position.set(this.segment.goal.x, this.segment.goal.y); // from goal
+    }
     this.color = color;
     this.speed = speed;
     this.posCounter = new counter(); // セルフカウンター
@@ -99,15 +145,26 @@ class point{
   }
   setting(){
     // segmentのendであるedgeによりinのsegmentIndexをoutのsegmentIndexに更新
-    let outSegmentIndex = this.segment.end.convertIndex(this.segment.index);
-    this.segment = graph.getSegment(outSegmentIndex);
+    //let outSegmentIndex = this.segment.end.convertIndex(this.segment.index);
+    //this.segment = graph.getSegment(outSegmentIndex);
+    //this.posCounter.setCounter(this.segment.interval, this.speed);
+    // step1: this.segmentとthis.moveFlagの情報から現在のedgeが分かるので
+    // step2: そのedgeにconvertしてもらうと次のsegmentが分かり
+    // step3: そのsegmentのstartのedgeのindexと現在のedgeのindexを比較することで
+    // step4: 次のmoveFlagが決まる。あとはposCounterにセットするだけ。
+    let currentEdge = (this.moveFlag ? this.segment.goal : this.segment.start);
+    let nextSegment = currentEdge.convert(this.segment);
+    console.log(currentEdge);
+    console.log(nextSegment);
+    this.moveFlag = (nextSegment.start.index === currentEdge.index ? true : false);
+    this.segment = nextSegment;
     this.posCounter.setCounter(this.segment.interval, this.speed);
   }
   update(){
     if(this.posCounter.increase()){
-      this.segment.calcPos(this.position, this.posCounter.cnt())
+      this.segment.calcPos(this.position, this.posCounter.cnt(), this.moveFlag) // フラグで方向指示
     }else{
-      this.setting(); // カウンターがfalseになったら次のsegmentを探す
+      this.setting(); // カウンターがfalseになったら次のsegmentを探す(edgeが自動的に指定してくれる)
     }
   }
   draw(){
@@ -121,35 +178,65 @@ class point{
 
 // まずデータからedgeを作り、次にedgeを元にしてitvDataからsegmentを生成する
 class edge{
-  constructor(i, x, y, inArray){
-    this.index = i;
+  constructor(x, y){
+    this.index = edge.index++;
     this.x = x;
     this.y = y;
-    this.in = inArray; // 入ってくるsegmentのindex番号
-    this.convert = []; // segmentの変更を司る
+    this.connected = []; // つながってるsegmentのリスト
+    this.multiple = 0; // つながってるsegmentの個数
+    this.diff = 1; // 次のsegmentを指定するときにいくつずらすか（デフォルトは1）
   }
-  setConvertData(outArray){
-    for(let k = 0; k < this.in.length; k++){
-      this.convert[this.in[k]] = outArray[k]; // というわけで、inとoutという2つの配列から作ると。
-    } // これを使うと、outとして新しいsetを渡すことでconvertの仕様を変更できるようになる。
+  getIndex(segment){
+    // 与えられたsegmentが手持ちのconnectedリストの何番目なのかを返す
+    for(let i = 0; i < this.multiple; i++){
+      if(this.connected[i].index === segment.index){ return i; }
+    }
+    return -1;
   }
-  convertIndex(inSegmentIndex){ // inSegment → outSegment
-    return this.convert[inSegmentIndex];
+  addSegment(segment){
+    // segmentの追加
+    this.connected.push(segment);
+    this.multiple++;
+  }
+  deleteSegment(segment){
+    // segmentの削除
+    let correctIndex = this.getIndex(segment);
+    if(correctIndex < 0){ return; } // みつからないとき
+    this.connected.splice(correctIndex, 1); // 該当するsegmentを削除
+  }
+  convert(segment){
+    // 次のsegmentを指定
+    let currentIndex = this.getIndex(segment);
+    if(currentIndex < 0){ return; } // みつからないとき
+    let nextIndex = (currentIndex + this.diff) % this.multiple; // this.diffだけずらしていく
+    console.log(nextIndex);
+    return this.connected[nextIndex]; // 次なるsegmentを返す
   }
 }
 
 // startからendまでitvカウントで到達
 class segment{
-  constructor(i, e1, e2, itv){
-    this.index = i;
-    this.start = e1;
-    this.end = e2;
-    this.interval = itv;
+  constructor(e1, e2){ // やっぱエッジで作らないとsettingで積む
+    this.index = segment.index++;
+    this.start = e1; // 一応向き付き（どっちからどっちへも行ける）
+    this.goal = e2;
+    this.interval = Math.sqrt((e1.x - e2.x) * (e1.x - e2.x) + (e1.y - e2.y) * (e1.y - e2.y)); // 距離でいい
   }
-  calcPos(position, cnt){
-    let vx = this.start.x + (this.end.x - this.start.x) * (cnt / this.interval);
-    let vy = this.start.y + (this.end.y - this.start.y) * (cnt / this.interval);
-    if(cnt >= this.interval){ vx = this.end.x; vy = this.end.y; }
+  calcPos(position, cnt, flag){
+    let value = cnt / this.interval; // 順方向
+    if(!flag){ value = 1 - value; }  // 逆方向
+    let vx = this.start.x + (this.goal.x - this.start.x) * value;
+    let vy = this.start.y + (this.goal.y - this.start.y) * value;
+    if(cnt >= this.interval){
+      if(flag){
+        vx = this.goal.x; vy = this.goal.y; // 順方向
+      }else{
+        vx = this.start.x; vy = this.start.y; // 逆方向
+      }
+    }
     position.set(vx, vy);
   }
 }
+
+edge.index = 0; // 頂点の連番
+segment.index = 0; // 辺の連番
