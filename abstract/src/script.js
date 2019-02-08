0363d0e495360a0bb1e32e6626a56db306a64383
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2019-02-09 06:34:05 +0900

    三角形追加

--- abstract/src/script.js
+++ abstract/src/script.js
@@ -1,11 +1,13 @@
 // 具体化しようよ
 'use strict';
 
+const HUB_RADIUS = 5;
+
 let graph;
 
 function setup(){
   createCanvas(400, 400);
-  angleMode(DEGREES);
+  //angleMode(DEGREES);
   graph = new entity();
   graph.loadData();
   graph.createGraph();
@@ -94,30 +96,47 @@ class straightFlow extends flow{
     pos.y = map(cnt, 0, this.span, this.from.y, this.to.y);
   }
   drawOrbit(gr){
+    gr.push();
     gr.strokeWeight(1.0);
     gr.line(this.from.x, this.from.y, this.to.x, this.to.y);
+    //gr.push();
+    gr.translate(this.from.x, this.from.y); // 矢印の根元に行って
+    let directionVector = createVector(this.to.x - this.from.x, this.to.y - this.from.y);
+    gr.rotate(directionVector.heading()); // ぐるんってやって行先をx軸正方向に置いて
+    let arrowSize = 7;
+    gr.translate(this.span - HUB_RADIUS - arrowSize, 0);
+    gr.fill(0);
+    gr.triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+    gr.pop();
   }
 }
 
 class circleFlow extends flow{
-  constructor(h1, h2, cx, cy, radius, ph1, ph2){
+  constructor(h1, h2, cx, cy, radius, rad1, rad2){
+    // rad1, rad2はふたつのラジアンで反時計回りに進む、はず。
+    // あー・・えっと、たとえば2π→π・・・あっ。。んー下がります。そうです。（下を回る）
+    // で、上がるときは0→πってやるんですよ。そうです。上がります。そうです。（上を回る）
+    // mapでやる。
     super(h1, h2);
     this.cx = cx;
     this.cy = cy;
     this.radius = radius;
-    this.ph1 = ph1; // 開始位相
-    this.ph2 = ph2; // 終了位相
-    this.span = ph2 - ph1;
+    this.rad1 = rad1; // 開始位相
+    this.rad2 = rad2; // 終了位相（補間で変化する）
+    this.span = radius * (rad2 - rad1); //ここを円弧の長さにします
   }
   calcPos(pos, cnt){
-    pos.x = this.cx + this.radius * cos(this.ph1 + cnt);
-    pos.y = this.cy + this.radius * sin(this.ph1 + cnt);
+    pos.x = this.cx + this.radius * cos(map(cnt, 0, this.span, this.rad1, this.rad2));
+    pos.y = this.cy + this.radius * sin(map(cnt, 0, this.span, this.rad1, this.rad2));
   }
   drawOrbit(gr){
     gr.strokeWeight(1.0);
-    gr.arc(this.cx, this.cy, 2 * this.radius, 2 * this.radius, this.ph1, this.ph2);
+    gr.arc(this.cx, this.cy, 2 * this.radius, 2 * this.radius, this.rad1, this.rad2);
   }
 }
+
+// class parabolaFlow{} 作成したい
+// 仕様としてはその・・
 
 class actor{
   constructor(h, speed){
@@ -136,6 +155,7 @@ class actor{
     if(!this.timer.getState()){ return; }
     this.timer.progress();
     this.move.calcPos(this.pos, this.timer.getCnt());
+    // 多分イージング入れるとしたら、ここ。加法的か乗法的か知らないけど。
     if(!this.timer.check()){ this.setting(); } // counter check.
   }
   display(){
@@ -181,19 +201,20 @@ class circle extends figure{
 }
 
 class square extends figure{
-  constructor(fillColor, size, rolling){
+  constructor(fillColor, size, rollingSpeed){
     super(fillColor);
     this.size = size;
-    this.rolling = rolling; // 回転するか否か
+    this.rollingSpeed = rollingSpeed; // 回転するか否か
     this.rotation = 0;
-    if(rolling){ this.rotation = random(360); }
+    if(rollingSpeed !== 0){ this.rotation = random(360); }
   }
   display(pos){
     push();
     translate(pos.x, pos.y);
     noStroke();
     fill(this.color);
-    rotate(this.rotation++);
+    this.rotation += this.rollingSpeed;
+    rotate(this.rotation);
     rect(-this.size * 0.5, -this.size * 0.5, this.size, this.size);
   }
 }
@@ -228,7 +249,7 @@ class entity{
     }, this)
     console.log(5);
     this.hubs.forEach(function(h){
-      this.baseGraph.ellipse(h.x, h.y, 10, 10);
+      this.baseGraph.ellipse(h.x, h.y, HUB_RADIUS * 2, HUB_RADIUS * 2); // ここをhubごとにdrawさせたい気持ちもある・・
     }, this)
     console.log(3);
   }
@@ -238,25 +259,40 @@ class entity{
     this.loadData();
     this.createGraph();
   }
+  registHub(posX, posY, n){
+    for(let i = 0; i < n; i++){ this.hubs.push(new hub(posX[i], posY[i])); }
+  }
+  registStraightFlow(inHubsId, outHubsId, n){
+    for(let i = 0; i < n; i++){
+      this.flows.push(new straightFlow(this.hubs[inHubsId[i]], this.hubs[outHubsId[i]]));
+      this.hubs[inHubsId[i]].outFlow.push(this.flows[i]);
+    }
+  }
+  setCircle(hubId, speed, selfColor, radius){
+    let mf = new movingCircle(this.hubs[hubId], speed, selfColor, radius);
+    this.movefigs.push(mf);
+  }
+  setSquare(hubId, speed, selfColor, size, rollingSpeed){
+    let mf = new movingSquare(this.hubs[hubId], speed, selfColor, size, rollingSpeed);
+    this.movefigs.push(mf);
+  }
 }
 
 // そのうち登録・・なんだっけregist？registメソッド作って簡単にするから待ってて
-
 function createPattern0(){
-  let posX = [100, 300, 300, 100];
-  let posY = [100, 100, 300, 300];
-  for(let i = 0; i < 4; i++){
-    graph.hubs.push(new hub(posX[i], posY[i]));
+  let posX = [];
+  let posY = [];
+  for(let x1 = 0; x1 < 5; x1++){
+    for(let x2 = 0; x2 <= x1; x2++){
+      posX.push(200 - 30 * x1 + 60 * x2);
+      posY.push(100 + 30 * sqrt(3) * x1);
+    }
   }
-  let inHubs = [0, 1, 2, 3];
-  let outHubs = [1, 2, 3, 0];
-  for(let i = 0; i < 4; i++){
-    graph.flows.push(new straightFlow(graph.hubs[inHubs[i]], graph.hubs[outHubs[i]]));
-    graph.hubs[inHubs[i]].outFlow.push(graph.flows[i]);
-  }
-  let mf = new movingCircle(graph.hubs[0], 2, color('red'), 10);
-  graph.movefigs.push(mf);
-  console.log(1);
+  graph.registHub(posX, posY, 15);
+  let inHubsId = [0, 1, 3, 6, 2, 4, 7, 5, 8, 9, 14, 9, 5, 2, 13, 8, 4, 12, 7, 11, 1, 3, 4, 6, 7, 8, 10, 11, 12, 13];
+  let outHubsId = [1, 3, 6, 10, 4, 7, 11, 8, 12, 13, 9, 5, 2, 0, 8, 4, 1, 7, 3, 6, 2, 4, 5, 7, 8, 9, 11, 12, 13, 14];
+  graph.registStraightFlow(inHubsId, outHubsId, 30);
+  graph.setCircle(0, 2, color('red'), 15);
 }
 
 function createPattern1(){
@@ -271,20 +307,20 @@ function createPattern1(){
     graph.flows.push(new straightFlow(graph.hubs[inHubs[i]], graph.hubs[outHubs[i]]));
     graph.hubs[inHubs[i]].outFlow.push(graph.flows[i]);
   }
-  let mf = new movingSquare(graph.hubs[0], 2, color('blue'), 10, true);
+  let mf = new movingSquare(graph.hubs[0], 2, color('blue'), 20, 0.1);
   graph.movefigs.push(mf);
 }
 
 function createPattern2(){
   for(let i = 0; i < 5; i++){
-    let x = 200 + 100 * cos(72 * i);
-    let y = 200 + 100 * sin(72 * i);
+    let x = 200 + 100 * cos(2 * i * PI / 5);
+    let y = 200 + 100 * sin(2 * i * PI / 5);
     graph.hubs.push(new hub(x, y));
   }
   for(let i = 0; i < 5; i++){
     graph.flows.push(new straightFlow(graph.hubs[i % 5], graph.hubs[(i + 1) % 5]));
     graph.hubs[i].outFlow.push(graph.flows[i]);
   }
-  let mf = new movingCircle(graph.hubs[0], 2, color('orange'), 10);
+  let mf = new movingCircle(graph.hubs[0], 2, color('orange'), 20);
   graph.movefigs.push(mf);
 }
